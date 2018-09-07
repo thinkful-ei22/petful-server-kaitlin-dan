37bb47f756ad685f7da22ad299d21604df607892
Kaitlin Bunn <kabunn1991@gmail.com>
2018-09-07 11:03:09 -0400

    added get endpoints

--- index.js
+++ index.js
@@ -22,6 +22,39 @@ app.use(
   })
 );
 
+// cat GET function
+
+app.get('/api/cat', (req,res) => {
+  // show the cat that is next in line to be adopted
+  const dummyCat = {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  };
+
+  return res.json(dummyCat); //return a cat
+})
+
+// dog GET function
+
+app.get('/api/dog', (req, res) => {
+  const dummyDog = {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }
+
+  return res.json(dummyDog); // return a dog
+})
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
